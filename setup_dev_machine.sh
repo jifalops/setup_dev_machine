8723044d646190acdae3ca2ac0260e60f07c3518
Jacob Phillips <jifalops@gmail.com>
2019-06-27 17:29:46 -0400

    careful path changes

--- setup_dev_machine.sh
+++ setup_dev_machine.sh
@@ -37,6 +37,7 @@ flutter
         Installs Flutter from the git repo. Also installs the "android" target.
 android
         Installs the Android command line tools, without Android Studio.
+
 node
         Installs nvm and the latest version of node/npm.
 anaconda
@@ -63,7 +64,19 @@ command_exists() {
 package_exists() {
   $(dpkg -l "$1" >/dev/null 2>&1) && echo 1
 }
-is_in_path() {
+install_packages() {
+  local to_install=()
+  for package in "$@"; do
+    package_exists "$package" || to_install+=("$package")
+  done
+  local len=${#to_install[@]}
+  if [ $len ]; then
+    echo "" >/dev/null
+    # sudo apt-get -y install ${to_install[@]}
+  fi
+  return $len
+}
+path_contains() {
   [[ $PATH == *"$1"* ]] && echo 1
 }
 
@@ -124,11 +137,6 @@ if [ -n "$install_dir" ]; then
   fi
 else
   install_dir="$HOME/tools"
-  # TODO move
-  # if [ ${has_target[flutter]} ] && [ -d "$install_dir/flutter" ]; then
-  #   echo "$install_dir/flutter already exists."
-  #   exit 1
-  # fi
   if [ ! -d "$install_dir" ]; then
     mkdir "$install_dir" || exit 1
   fi
@@ -152,10 +160,37 @@ for target in "${targets[@]}"; do
   fi
 done
 
+# Ensure targets are not already installed unless --force was supplied.
+# Doing this first lets the command fail right away if there is a problem.
+declare -A target_installed
+target_installed[vscode]=$(command_exists code)
+target_installed[flutter]=$(command_exists flutter)
+target_installed[android]=$(command_exists sdkmanager)
+# target_installed[studio]=$(command_exists studio.sh)
+target_installed[node]=$(command_exists node)
+target_installed[anaconda]=$(command_exists anaconda)
+target_installed[miniconda]=$(path_contains miniconda)
+target_installed[pip]=$(command_exists pip3)
+
+if [ ! $force_install ]; then
+  for target in "${targets[@]}"; do
+    if [ target_installed[$target] ]; then
+      echo "Target '$target' is already installed. Use --force to override."
+      exit 1
+    fi
+  done
+fi
+
+# Exit if the flutter repo dir exists and is not empty.
+if [ ${has_target[flutter]} ] && [ -d "$install_dir/flutter" ] && [ "$(ls -A $install_dir/flutter)" ]; then
+  echo "$install_dir/flutter already exists and is not empty."
+  exit 1
+fi
 
 #
-# Installers
+# Fatal errors are accounted for, on to the installers.
 #
+PATH_CHANGES=""
 
 # VS Code with settings-sync
 if [ ${has_target[vscode]} ]; then
@@ -165,25 +200,23 @@ if [ ${has_target[vscode]} ]; then
   echo "See https://code.visualstudio.com/docs/setup/linux"
   echo "======================================================="
   echo
-  if [ $(command -v code) ]; then
-    echo "WARNING: VSCode is already installed."
-  fi
-  sudo apt-get -y install gnupg
+  install_packages gnupg
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
   sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
   sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-  sudo apt-get -y install apt-transport-https
+  install_packages apt-transport-https
   sudo apt-get update
-  sudo apt-get -y install code # or code-insiders
+  install_packages code # or code-insiders
 
   # Extensions
+  # TODO install extensions only if necessary
   code --install-extension dart-code.flutter --force
   code --install-extension ms-python.python --force
   code --install-extension shan.code-settings-sync --force
 
   if [ -n "$code_settings_gist" ]; then
-    sudo apt-get -y install jq
+    install_packages jq
     settings_file="$HOME/.config/Code/User/settings.json"
     sync_file="$HOME/.config/Code/User/syncLocalSettings.json"
     default_sync_settings="{ \"sync.gist\": \"$code_settings_gist\", \"sync.autoDownload\": true, \"sync.autoUpload\": true, \"sync.quietSync\": true }"
@@ -206,33 +239,31 @@ fi
 # Flutter
 if [ ${has_target[flutter]} ]; then
   echo
-  echo "=========================================================================="
+  echo "==========================================================="
   echo "Setting up Flutter from GitHub (master)"
   echo "See https://flutter.dev/docs/development/tools/sdk/releases"
+  echo "==========================================================="
   echo
-  if [ $(command -v flutter) ]; then
-    echo "WARNING: flutter is already installed."
-  fi
   git clone -b master https://github.com/flutter/flutter.git "$install_dir/flutter"
   "$install_dir/flutter/bin/flutter" --version
 
-  sudo apt-get -y install lib32stdc++6 make clang
+  install_packages lib32stdc++6 make clang
 
-  export PATH="$PATH:$install_dir/flutter/bin:$install_dir/flutter/bin/cache/dart-sdk/bin:$HOME/.pub-cache/bin"
-  PATH_CHANGES+="$install_dir/flutter/bin:$install_dir/flutter/bin/cache/dart-sdk/bin:$HOME/.pub-cache/bin"
+  if [ ! path_contains "$install_dir/flutter/bin" ]; then
+    export PATH="$PATH:$install_dir/flutter/bin:$install_dir/flutter/bin/cache/dart-sdk/bin:$HOME/.pub-cache/bin"
+    PATH_CHANGES+="$install_dir/flutter/bin:$install_dir/flutter/bin/cache/dart-sdk/bin:$HOME/.pub-cache/bin"
+  fi
 fi
 
 # Android SDK and tools
 if [ ${has_target[flutter]} ] || [ ${has_target[android]} ]; then
   echo
-  echo "==================================================="
+  echo "======================================================="
   echo "Setting up the Android SDK (without Android Studio)"
   echo "See https://developer.android.com/studio/#command-tools"
+  echo "======================================================="
   echo
-  if [ $(command -v adb) ] && [ $(command -v sdkmanager) ] && [ $(command -v emulator) ]; then
-    echo "WARNING: android tools are already installed."
-  fi
-  sudo apt-get -y install default-jre default-jdk wget
+  install_packages default-jre default-jdk wget unzip
 
   if [ -z "$ANDROID_HOME" ]; then
     export ANDROID_HOME="$install_dir/android"
@@ -246,8 +277,10 @@ if [ ${has_target[flutter]} ] || [ ${has_target[android]} ]; then
   unzip sdk-tools-linux*.zip*
   rm sdk-tools-linux*.zip*
 
-  export PATH="$PATH:$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools"
-  PATH_CHANGES+=':$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools'
+  if [ ! path_contains "$ANDROID_HOME/tools" ]; then
+    export PATH="$PATH:$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools"
+    PATH_CHANGES+=':$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools'
+  fi
 
   # Squelches a repeated warning
   mkdir "$HOME/.android" >/dev/null 2>&1
@@ -257,9 +290,12 @@ if [ ${has_target[flutter]} ] || [ ${has_target[android]} ]; then
   # TODO pass this as a version-dependent variable.
   sdkmanager "build-tools;28.0.3" "emulator" "tools" "platform-tools" "platforms;android-28" "extras;google;google_play_services" "extras;google;webdriver" "system-images;android-28;google_apis_playstore;x86_64"
 
-  export PATH="$PATH:$ANDROID_HOME/platform-tools"
-  PATH_CHANGES+=':$ANDROID_HOME/platform-tools'
+  if [ ! path_contains "$ANDROID_HOME/platform-tools" ]; then
+    export PATH="$PATH:$ANDROID_HOME/platform-tools"
+    PATH_CHANGES+=':$ANDROID_HOME/platform-tools'
+  fi
 
+  # reset pwd
   cd "$install_dir"
 fi
 
@@ -269,10 +305,8 @@ if [ ${has_target[node]} ]; then
   echo "======================================================="
   echo "Installing Node and npm via Node Version Manager (nvm)"
   echo "See https://github.com/nvm-sh/nvm/blob/master/README.md"
+  echo "======================================================="
   echo
-  if [ $(command -v nvm) ]; then
-    echo "WARNING: nvm is already installed."
-  fi
   curl -o- "$NVM_SETUP_SCRIPT" | bash
 
   export NVM_DIR="$HOME/.nvm"
@@ -290,13 +324,10 @@ if [ ${has_target[anaconda]} ]; then
   echo
   echo "================================"
   echo "Installing Anaconda for Python 3"
+  echo "================================"
   echo
-  if [ $(command -v anaconda) ]; then
-    echo "WARNING: anaconda already installed."
-  fi
-  if [ ! $(command -v wget) ]; then
-    sudo apt-get -y install wget
-  fi
+  install_packages wget
+
   wget "$ANACONDA_SETUP_SCRIPT" -O "$install_dir/anaconda.sh"
   bash "$install_dir/anaconda.sh" -b -p "$install_dir/anaconda"
   rm "$install_dir/anaconda.sh"
@@ -305,59 +336,63 @@ if [ ${has_target[anaconda]} ]; then
   if [ ! -d "$HOME/.local/share/applications" ]; then
     mkdir "$HOME/.local/share/applications"
   fi
-  cat <<EOF >"$HOME/.local/share/applications/spyder.desktop"
+  cat <<EOF >"$HOME/.local/share/applications/spyder3.desktop"
 [Desktop Entry]
-Name=Spyder
-GenericName=Text Editor
-Exec=$install_dir/anaconda/bin/spyder
-Icon=$install_dir/anaconda/lib/python3.7/site-packages/spyder/images/spyder.svg
 Type=Application
-StartupNotify=false
+Version=1.0
+Name=Spyder3
+GenericName=Spyder3
+Comment=The Scientific Python Development Environment - Python 3
+Icon=spyder3
+TryExec=spyder
+Exec=spyder %F
+Terminal=false
+MimeType=text/x-python;
+Categories=Development;Science;IDE;Qt;
+Keywords=Development;Science;IDE;Qt;
+StartupNotify=true
 StartupWMClass=Spyder
-Categories=Utility;TextEditor;Development;IDE;
-MimeType=text/plain;inode/directory;
-Actions=new-empty-window;
-Keywords=spyder;
-
-X-Desktop-File-Install-Version=0.23
-
-[Desktop Action new-empty-window]
-Name=New Empty Window
-Exec=$install_dir/anaconda/bin/spyder
-Icon=$install_dir/anaconda/lib/python3.7/site-packages/spyder/images/spyder.svg
 EOF
+
+  if [ ! -d "$HOME/.local/share/icons" ]; then
+    mkdir "$HOME/.local/share/icons"
+  fi
+  cp "$install_dir/anaconda/share/icons/spyder3.png" "$HOME/.local/share/icons/spyder3.png"
+
   sudo update-desktop-database
 
-  export PATH="$PATH:$install_dir/anaconda/bin:$install_dir/anaconda/condabin"
-  PATH_CHANGES+=":$install_dir/anaconda/bin:$install_dir/anaconda/condabin"
+  if [ ! path_contains "$install_dir/anaconda" ]; then
+    export PATH="$PATH:$install_dir/anaconda/bin:$install_dir/anaconda/condabin"
+    PATH_CHANGES+=":$install_dir/anaconda/bin:$install_dir/anaconda/condabin"
+  fi
 fi
 
 # Miniconda.
 if [ ${has_target[miniconda]} ]; then
   echo
-  echo "================================"
+  echo "================================="
   echo "Installing Miniconda for Python 3"
+  echo "================================="
   echo
-  if [ $(command -v conda) ] && [ ! $(command -v anaconda) ]; then
-    echo "WARNING: miniconda already installed."
-  fi
-  if [ ! $(command -v wget) ]; then
-    sudo apt-get -y install wget
-  fi
+  install_packages wget
   wget "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" -O "$install_dir/miniconda.sh"
   bash "$install_dir/miniconda.sh" -b -p "$install_dir/miniconda"
   rm "$install_dir/miniconda.sh"
-  export PATH="$PATH:$install_dir/miniconda/bin:$install_dir/miniconda/condabin"
-  PATH_CHANGES+=":$install_dir/miniconda/bin:$install_dir/miniconda/condabin"
+
+  if [ ! path_contains "$install_dir/miniconda" ]; then
+    export PATH="$PATH:$install_dir/miniconda/bin:$install_dir/miniconda/condabin"
+    PATH_CHANGES+=":$install_dir/miniconda/bin:$install_dir/miniconda/condabin"
+  fi
 fi
 
 # pip for Python 3
 if [ ${has_target[pip]} ] && [ ! ${has_target[anaconda]} ] && [ ! ${has_target[miniconda]} ]; then
   echo
-  echo "================================"
+  echo "==========================="
   echo "Installing pip for Python 3"
+  echo "==========================="
   echo
-  sudo apt-get -y install python3-pip
+  install_packages python3-pip
 fi
 
 # ChromeOS specific
@@ -366,12 +401,15 @@ if [ -d /mnt/chromeos ] && [ ! -e "$HOME/Downloads" ]; then
 fi
 
 # Extras
-type la >/dev/null 2>&1 || echo 'alias la="ls -a"' >>"$HOME/.profile"
-type ll >/dev/null 2>&1 || echo 'alias ll="ls -la"' >>"$HOME/.profile"
-sudo apt-get -y install software-properties-common
+command_exists la || echo 'alias la="ls -a"' >>"$HOME/.profile"
+command_exists ll || echo 'alias ll="ls -la"' >>"$HOME/.profile"
+install_packages software-properties-common
 
 # Finishing up
-echo "export PATH=\"$PATH_CHANGES:\$PATH\"" >>"$HOME/.profile"
+
+if [ -n $PATH_CHANGES ]; then
+  echo "export PATH=\"$PATH_CHANGES:\$PATH\"" >>"$HOME/.profile"
+fi
 
 if [ ${has_target[flutter]} ]; then
   flutter doctor
