0ee521d28eacf88c55395af12ae03d3c05654a2b
Jacob Phillips <jifalops@gmail.com>
2019-06-28 03:26:31 -0400

    fix nvm 2

--- setup_dev_machine.sh
+++ setup_dev_machine.sh
@@ -321,7 +321,7 @@ if [ ${has_target[node]} ]; then
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-  source "$HOME/.profile"
+  source "$NVM_DIR/nvm.sh"
 
   nvm install node
 
